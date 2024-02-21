b32eb6ce26f6b85c5eeab2e4e005b209eeb60adc
Antonio0806 <antonio0806123@gmail.com>
2024-02-21 22:24:12 +0100

    deleted constants example

--- src/constants.example.ts
+++
@@ -1,7 +0,0 @@
-//Secrets
-export const JWT_SECRET: string = 'secret';
-//telegram notifications
-export const TG_BOT_TOKEN: string = '';
-export const CHAT_ID: string = '';
-//SSH
-export const SSH_SERVER: string = '123.1234.1234'
